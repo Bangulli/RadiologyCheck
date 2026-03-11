d4c556dbe8dfd7541dbf8ec7d85cd15a318058df
Bangulli <lorenz.achim.kuhn@gmail.com>
2026-03-11 10:28:31 +0000

    Enable resumption of process

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from src.inference.inference import infer
   
 #------------------------------------------------ MAIN PARSER ------------------------------------------------#
 p = argparse.ArgumentParser()
-p.add_argument("-model", default="medgemma", help="which model to use")
 p.add_argument("-dir", default="outputs/run", help="A directory where the prompt as well as the model output are stored.")
+p.add_argument("--resume", action="store_true", help="When used will load the prompt from the specified directory and resume the refining loop.")
 
 if __name__ == "__main__":
     args = p.parse_args()
